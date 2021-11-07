739af42d848e7f22a8e02df7163c68e2aead10d8
Anthony Jimenez <tony6776a@gmail.com>
2021-11-06 22:25:56 -0400

    created strings and arrays for our Math.random to pull options from later in the generatePassword function. Also created an eventListener for a new GeneratePasswordButton so that on click it will call writePassword to start the whole process

--- assets/js/script.js
+++ assets/js/script.js
@@ -109,4 +109,3 @@ function writePassword(){
     return;
 }
 
-generate
--- assets/js/script2.js
+++ assets/js/script2.js
@@ -1,2 +1,29 @@
+console.log("script2 loaded correctly")
 var generatePasswordButton = document.getElementById("generate");
 
+
+function generatePassword(){
+    //All possible string the password could be made of
+    var specialCharstr ="~`!@#$%^&*()_-+=";
+    var upperCaseLetterStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCaseLetterStr = "abcdefghijklmnopqrstuvwxyz";
+    var numberStr = "0123456789";
+    //Turn the strings into Arrays so that we can pull individual
+    //characters out of the strings for the password randomly
+    var specialCharArr = specialCharStr.split("");
+    var upperCaseLetterArr = upperCaseLetterStr.split("");
+    var lowerCaseLetterArr = lowerCaseLetterStr.split("");
+    var numberArr = numberStr.split("");
+
+
+}
+
+function writePassword(){
+    var password = generatePassword();//Hands over password from generate Password function
+    var passwordTextEl = document.getElementById("password");
+
+    passwordTextEl.value = password;
+    return;
+}
+
+generatePasswordButton.addEventListener("click", writePassword);
